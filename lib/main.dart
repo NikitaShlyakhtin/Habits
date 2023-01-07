7d8651ffeaefed1f8f8253e48311d012b46aba01
Fraus <nikitashliahtin@mail.ru>
2023-01-07 15:50:46 +0300

    firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,19 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:habit_tracker/data/habit.dart';
 import 'package:habit_tracker/data/habit_list.dart';
 import 'package:habit_tracker/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:habit_tracker/firebase/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await Hive.initFlutter();
   Hive.registerAdapter(HabitAdapter());
   Box box = await Hive.openBox('box');
+
   runApp(ChangeNotifierProvider(
     create: (context) => HabitList(),
     child: const MyApp(),
