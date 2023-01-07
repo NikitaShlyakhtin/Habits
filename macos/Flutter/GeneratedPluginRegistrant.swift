b3a453189c73e391f8a382dbb0a3d85ca99f4ba4
Fraus <nikitashliahtin@mail.ru>
2023-01-07 17:33:57 +0300

    Revert "firebase initialization"
    This reverts commit 7d8651ffeaefed1f8f8253e48311d012b46aba01.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import dynamic_color
-import firebase_core
 import path_provider_macos
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
